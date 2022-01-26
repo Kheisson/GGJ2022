a73fe812c37e00d1f622ffb62fb20ddf6fee82fa
Kheisson <igor.kheison@gmail.com>
2022-01-26 19:03:46 +0200

    add mock loading scene

---
+++ Assets/Scripts/UI Operations/LoadScene.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private Image loadingBarImage;
+
+    #endregion
+
+    #region Methods
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(LoadSceneAsync());
+    }
+    
+    private IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            loadingBarImage.fillAmount = asyncOperation.progress;
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    #endregion
+}
