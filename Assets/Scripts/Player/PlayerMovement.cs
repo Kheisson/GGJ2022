a2384a8c8475e6714c62ad8542cd9fd28bed3a88
Kheisson <igor.kheison@gmail.com>
2022-02-01 23:25:53 +0200

    removed new input system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     #region Consts
-
-    private const string HorizontalInput = "Horizontal";
-    private const string VerticalInput = "Vertical";
 
     #endregion
 
@@ -17,20 +13,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speed = 10f;
 
     #endregion
-    
+
     #region Methods
-
-    private void Update()
-    {
-        HandlePlayerInput();
-    }
-
-    private void HandlePlayerInput()
-    {
-        var horizontalInput = Input.GetAxis(HorizontalInput) * Time.deltaTime;
-        var verticalInput = Input.GetAxis(VerticalInput) * Time.deltaTime;
-        transform.Translate(new Vector3(horizontalInput,verticalInput,transform.position.z) * _speed);
-    }
 
     #endregion
 }
