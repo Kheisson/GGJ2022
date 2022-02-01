1ce1a563b896224d652c5d8c399a7e9e0cb437b2
Kheisson <igor.kheison@gmail.com>
2022-02-01 21:23:54 +0200

    add device simulator

--- Assets/Scripts/UI Operations/LoadScene.cs
+++ Assets/Scripts/UI Operations/LoadScene.cs
@@ -17,7 +17,7 @@ public class LoadScene : MonoBehaviour
 
     private IEnumerator Start()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         StartCoroutine(LoadSceneAsync());
     }
     
