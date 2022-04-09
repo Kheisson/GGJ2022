e2f2f86a704028da44d5be035e3ba8de592b093f
Kheisson <igor.kheison@gmail.com>
2022-04-09 13:17:57 +0300

    performance tweak for map button

--- Assets/Scripts/Level/MapButton.cs
+++ Assets/Scripts/Level/MapButton.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AirFishLab.Fixes;
 using AirFishLab.ScrollingList;
 using Core;
@@ -8,12 +9,12 @@ namespace Level
     public class MapButton : MonoBehaviour
     {
         [SerializeField] private CircularScrollingList circularScrollingList;
-
+        private List<IntListBox> _listBoxes = new List<IntListBox>();
 
         private void Start()
         {
+            Init();
             InvokeRepeating(nameof(UpdateButtonStates), 0.1f, 0.1f);
-            Time.timeScale = 1f;
         }
 
         public void LoadLevel(int level)
@@ -23,9 +24,18 @@ namespace Level
             print($"Loading scene: {levelNumber.ToString()}");
         }
 
+        private void Init()
+        {
+            Time.timeScale = 1f;
+            if (_listBoxes.Count != 0) return;
+            foreach (var box in circularScrollingList.listBank.GetComponentsInChildren<IntListBox>())
+            {
+                _listBoxes.Add(box);
+            }
+        }
         private void UpdateButtonStates()
         {
-            foreach (var box in circularScrollingList.listBank.GetComponentsInChildren<IntListBox>())
+            foreach (var box in _listBoxes)
             {
                 var mode = GameManager.Instance.GetClearedLevel(int.Parse(box.Text));
                 box.TurnButton(mode);
